9bec0cb5dc8b9e248a9f4d803a6236e1da5fa385
Racklyn Sotero <racklynsotero@gmail.com>
2021-08-10 14:48:44 -0300

    Changes on handTrackingModule.py

--- handTrackingModule.py
+++ handTrackingModule.py
@@ -50,32 +50,32 @@ class handDetector():
         return lmList
 
 
-# def main():
-#     pTime = 0 #previous
-#     cTime = 0 #current
-#     cap = cv2.VideoCapture(0)
-#     # New object:
-#     detector = handDetector()
+def main():
+    pTime = 0 #previous
+    cTime = 0 #current
+    cap = cv2.VideoCapture(0)
+    # New object:
+    detector = handDetector()
 
-#     while True:
-#         success, img = cap.read()
-#         img = detector.findHands(img)
-#         lmList = detector.findPosition(img)
-#         if len(lmList)!= 0:
-#             print(lmList[4]) #ponto 4 da mão (dedão)    
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList)!= 0:
+            print(lmList[4]) #ponto 4 da mão (dedão)    
 
-#         cTime = time.time()
-#         fps = 1/(cTime - pTime)
-#         pTime = cTime
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
 
-#         cv2.putText(img, str(int(fps))+" fps", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 3)
+        cv2.putText(img, str(int(fps))+" fps", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 3)
 
-#         # Mostrando imagem:
-#         cv2.imshow("Image", img) #cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
-#         cv2.waitKey(1)
+        # Mostrando imagem:
+        cv2.imshow("Image", img) #cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
+        cv2.waitKey(1)
 
 
 
-# # se este script/arquivo está sendo executado
-# if __name__ == "__main__":
-#     main()
+# se este script/arquivo está sendo executado
+if __name__ == "__main__":
+    main()
